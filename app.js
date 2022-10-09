d0f1451ea3aecbd1d5a1790440153abf6c88c412
uxolrv <uxolrv@gmail.com>
2022-10-09 22:08:25 +0900

    :sparkles: #4.4: get username

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
-
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-
-    console.log(username);
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
